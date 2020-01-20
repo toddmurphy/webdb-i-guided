b35fab636ddb887ec04f64d3990e353280fe09de
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-01-20 11:54:59 +0100

    ip

--- data/db-config.js
+++ data/db-config.js
@@ -3,3 +3,6 @@ const knex = require('knex');
 const config = require('../knexfile.js');
 
 module.exports = knex(config.development);
+
+// import this thing from someplace else
+// and do sql with it in JS
--- knexfile.js
+++ knexfile.js
@@ -12,4 +12,6 @@ module.exports = {
       directory: './data/seeds',
     },
   },
+  production: {
+  }
 };
